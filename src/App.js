75fc00f780c72a8df48f12e5bd58fe3787979ce9
jgrigorian23 <jakepgrigorian@gmail.com>
2024-02-03 22:41:08 -0800

    Everything good, alignment

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import './App.css';
 import Tools from './Tools.js';
 
 function App() {
+  const backClick = () => {
+    alert('The back button has been clickified')
+  };
+
+  const finishClick = () => {
+    alert('The finish button has been clickified')
+  };
+
+  const pfpClick = () => {
+    alert('The beautiful profile picture is clickified')
+  }
 
   return (
     <div className="App">
       <header className="App-header">
         <p className="logo-text">hive</p>
-        <button className="profile-button">
+        <button className="profile-button" onClick={pfpClick}>
           <img src="lavalabprofile.jpg" alt="Profile"/>
         </button>
       </header>
@@ -16,8 +27,8 @@ function App() {
       <div className="tools-container">
         <Tools />
       </div>
-      <button className="back-button">Back</button>
-      <button className="finish-button">Finish</button>
+      <button className="back-button" onClick={backClick}>Back</button>
+      <button className="finish-button" onClick={finishClick}>Finish</button>
     </div>
   );
 }
